0d400aab735f6181ecd33cae88018f2265b3100f
AlexLitvino <litvinov.aleks@gmail.com>
2026-06-18 15:32:11 +0300

    added handling console errors and alerts

--- page_objects/application.py
+++ page_objects/application.py
@@ -1,5 +1,7 @@
+import logging
+
 from playwright.sync_api import Browser
-from playwright.sync_api import Request, Route
+from playwright.sync_api import Request, Route, ConsoleMessage, Dialog
 
 from page_objects.test_cases import TestCases
 from page_objects.demo_pages import DemoPages
@@ -14,6 +16,17 @@ class App:
         self.base_url = base_url
         self.test_cases = TestCases(self.page)
         self.demo_pages = DemoPages(self.page)
+
+        def console_handler(message: ConsoleMessage):
+            if message.type == "error":
+                logging.error(f"page: {self.page.url}, console error: {message.text}")
+
+        def dialog_handler(dialog: Dialog):
+            logging.warning(f"page: {self.page.url}, dialog test: {dialog.message}")
+            dialog.accept()
+
+        self.page.on('console', console_handler)
+        self.page.on('dialog', dialog_handler)
 
     def goto(self, endpoint: str, use_base_url=True):
         if use_base_url:
