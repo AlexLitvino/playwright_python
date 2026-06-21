5ca236baf9c3b86c55b00fe7be402df20223b22e
AlexLitvino <litvinov.aleks@gmail.com>
2026-06-21 14:41:36 +0300

    locator method

--- conftest.py
+++ conftest.py
@@ -187,3 +187,42 @@ def load_config(file: str) -> dict:
     config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
     with open(config_file) as cfg:
         return json.loads(cfg.read())
+
+
+# ######################################################################################################################
+
+SLO_MO = None
+HEADLESS = False
+
+
+@pytest.fixture(scope='module')
+def chromium():
+    """
+    Main fixture. Creates Chrome browser and yields it into tests
+    Shows how to work with docs
+    """
+    with sync_playwright() as p:
+        chromium = p.chromium.launch(headless=HEADLESS, slow_mo=SLO_MO)
+        yield chromium
+        chromium.close()
+
+
+@pytest.fixture(scope='module')
+def page(chromium):
+    # base url provided for context, so goto method can user only endpoint
+    context = chromium.new_context(permissions=["geolocation"],
+                                   geolocation={"latitude": 48.8, "longitude": 2.3},
+                                   base_url='http://127.0.0.1:8000')
+    page = context.new_page()
+    yield page
+    page.close()
+    context.close()
+
+
+@pytest.fixture(scope='module')
+def alice(page):
+    page.goto('')
+    page.fill('#id_username', 'alice')
+    page.fill('id=id_password', 'Qamania123')
+    page.click('text="Login"')
+    return page
